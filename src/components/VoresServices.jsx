8281d144409bfb55341371525ac04e90ce12ed39
Occamies <lasseknudsen01@hotmail.com>
2024-03-12 09:51:56 +0100

    Footer start

--- src/components/VoresServices.jsx
+++ src/components/VoresServices.jsx
@@ -6,6 +6,7 @@ import useRequestData from "../hooks/useRequestData"
 
 
 
+
 const VoresService = () => {
 
 
--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import Error from '../components/Error';
+import Loader from "../components/Loader"
+import React, { useEffect } from "react"
+import useRequestData from "../hooks/useRequestData"
 
 const Footer = () => {
+
+
+  const { data, isLoading, error, makeRequest } = useRequestData()
+
+  useEffect(() => {
+    makeRequest('http://localhost:5333/contactinformation')
+  }, [])
+
+
   return (
     <footer className='footer-container'>
+      {isLoading && <Loader />}
+      {error && <Error />}
+      <div>
+        <img src="/assets/logo.png" alt="Logo" />
+        <p>Som medlem af Elinstallatørernes Landsorganisation, ELFO. er vi tilsluttet et ankernævn og en garantiordning</p>
+      </div>
+
     </footer>
   )
 }
