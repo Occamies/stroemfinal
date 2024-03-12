639b13e791222aa9c508ec9c65ad76b61f00f0b7
Occamies <lasseknudsen01@hotmail.com>
2024-03-12 13:07:14 +0100

    footer done

---
+++ src/components/Nyhedsbrev.jsx
@@ -0,0 +1,40 @@
+import Error from '../components/Error';
+import Loader from "../components/Loader"
+import React, { useEffect } from "react"
+import useRequestData from "../hooks/useRequestData"
+
+
+const Nyhedsbrev = () => {
+
+
+  const { data, isLoading, error, makeRequest } = useRequestData()
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    let fd = new FormData(e.target)
+    makeRequest('http://localhost:5333/newssubscription',
+    {
+      "Content-Type":""
+    }, null, "POST", fd
+    )
+    console.log(formData)
+    e.target.reset();//empty inputs
+  }
+
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      {error && <Error />}
+      <form name='addNewsletter' onSubmit={handleSubmit} className='newsletterForm'>
+        <input type="email" name="email" id="mail" placeholder='Din Email' />
+        <button type='submit' className='btn'>Tilmeld</button>
+
+      </form>
+    </>
+  )
+}
+
+export default Nyhedsbrev;
--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,6 +2,16 @@ import Error from '../components/Error';
 import Loader from "../components/Loader"
 import React, { useEffect } from "react"
 import useRequestData from "../hooks/useRequestData"
+import { NavLink } from 'react-router-dom'
+
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaVimeoV } from "react-icons/fa";
+import Nyhedsbrev from '../components/Nyhedsbrev';
+
+
+
 
 const Footer = () => {
 
@@ -14,15 +24,49 @@ const Footer = () => {
 
 
   return (
-    <footer className='footer-container'>
-      {isLoading && <Loader />}
-      {error && <Error />}
-      <div>
-        <img src="/assets/logo.png" alt="Logo" />
-        <p>Som medlem af Elinstallatørernes Landsorganisation, ELFO. er vi tilsluttet et ankernævn og en garantiordning</p>
-      </div>
-
-    </footer>
+    <>
+      <footer className='footer-container'>
+        {isLoading && <Loader />}
+        {error && <Error />}
+        <div>
+          <img src="/assets/logo.png" alt="Logo" />
+          <p>Som medlem af Elinstallatørernes Landsorganisation, ELFO. er vi tilsluttet et ankernævn og en garantiordning</p>
+        </div>
+        <div>
+          <h5>Link</h5>
+          <ul className='footer-links'>
+            <li>
+              <NavLink> FAQ</NavLink>
+            </li>
+            <li><NavLink> Om os</NavLink></li>
+            <li><NavLink> Kontakt os</NavLink></li>
+            <li><NavLink> Services</NavLink></li>
+          </ul>
+        </div>
+        <div>
+          <h5>Kontakt os</h5>
+          <ul>
+            <li>Adresse: {data?.address}</li>
+            <li>Telefon: {data?.phone}</li>
+            <li>Email: {data?.email}</li>
+          </ul>
+        </div>
+        <div>
+          <h5>Nyhedsbrev</h5>
+          <p>Tilmeld dig vores nyhedsbrev her</p>
+          <Nyhedsbrev/>
+        </div>
+      </footer>
+      <section className="copyright">
+        <p><span>Strøm</span> © 2017 All Right Reserved</p>
+        <div className="some-footer">
+          <a href="#"><FaFacebookF /></a>
+          <a href="#"><FaLinkedinIn /></a>
+          <a href="#"><FaTwitter /></a>
+          <a href="#"><FaVimeoV /></a>
+        </div>
+      </section>
+    </>
   )
 }
 
